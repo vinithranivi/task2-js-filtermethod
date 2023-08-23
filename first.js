708ea65d19b758c71dbe666f998429fd9bf1fccb
vinithranivi <128589178+vinithranivi@users.noreply.github.com>
2023-08-23 16:24:25 +0530

    Add files via upload
    Filter() method

---
+++ first.js
@@ -0,0 +1,13 @@
+numbers =[1,2,3,4,5,6,7,8,9,10];
+const odds =numbers.filter((num)=>num%2==1);
+console.log(odds);
+var n =[1,3,5,7,9]
+function cubeof(n){
+    let cube
+    cube = n*n*n;
+    console.log(cube)
+}
+cubeof(n)
+
+
+
